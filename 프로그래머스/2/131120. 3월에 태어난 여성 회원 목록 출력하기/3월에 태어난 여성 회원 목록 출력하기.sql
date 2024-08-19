792d52ec16f0a29300bfe11f613a8cd1c1b026f9
Lee Min Kyung <minkyunglee1012@gmail.com>
2024-08-19 11:08:33 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+select MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH
+from MEMBER_PROFILE
+where month(DATE_OF_BIRTH) = 3 and TLNO is not null and GENDER = 'W'
+order by MEMBER_ID asc;
